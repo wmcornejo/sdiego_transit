4781ab4282a776c5dd05873d22f3037dc99baee2
William Cornejo <williamcornejo@Williams-MacBook-Air-the-scnd.local>
2025-08-17 20:26:11 -0400

    scrollable, more structure

--- src/components/MapView.vue
+++ src/components/MapView.vue
@@ -1,21 +1,53 @@
 <template>
-  <h1>San Diego Transit Map</h1>
-  <p>Hover over a tract to see the number of transit stops within it.</p>
-  <p>Data Source: <a href="https://data.sandiego.gov/datasets/?">San Diego Datasets</a></p>
-  <div class="map-row">
-    <div id="map1" class="map">
-      <div class="map-overlay" id="legend">
+  <div class="main-content">
+    <h1 class="display-4 mb-3">San Diego Transit Map</h1>
+    <h2><i>Transit in America's Finest City</i></h2>
 
-        <strong>Legend</strong>
-      </div>
+    <p>Transit is important in cities. In San Diego there is the trolley and bus
+      system. This map shows the transit routes and stops in San Diego, as well as
+      the number of transit stops per census tract. The census tracts are colored
+      by the number of transit stops they contain.
+    </p>
+
+    My map ideas are:
+    <ul>
+      <li>To create an intro map, showing which areas have the most transit stops and which don't</li>
+      <li>To create a ranked score based on proximity to stop, grocery, poverty levels, and car ownership</li>
+      <li>How long is your commute to work map?</li>
+    </ul>
+
+    <div id="map1" class="map">
+      <div class="map-overlay" id="legend"></div>
     </div>
+
+    <h2>Transit Challenges</h2>
+    <p>Despite San Diego's beautiful weather, our transit system has challenges.
+      Many communities lack direct access to a nearby stop. Equity and access
+      remain ongoing concerns.
+    </p>
+
+    <p>The map above shows the number of transit stops in each census tract.
+      Areas with fewer stops may face greater challenges in accessing public transit.
+    </p>
+
+    <p>The trolley and bus routes are not as extensive as they could be, especially
+      in suburban areas. This can lead to longer commutes and less reliable service.
+    </p>
+
+    <p>Improving transit access is crucial for reducing traffic congestion and
+      promoting sustainable transportation options.
+    </p>
   </div>
 </template>
+
 
 <script setup>
 import mapboxgl from 'mapbox-gl'
 import { onMounted } from 'vue'
 import * as turf from "@turf/turf";
+
+
+
 
 // Your token
 mapboxgl.accessToken = 'pk.eyJ1Ijoid202MjUiLCJhIjoiY21kc3RwZ3ZjMHdkMzJqcG9vbmRrcWxvdCJ9.QDO6nEgb2Ht2F3iAGtG_xw'
@@ -24,8 +56,8 @@ onMounted(() => {
   const map1 = new mapboxgl.Map({
     container: 'map1',
     style: 'mapbox://styles/mapbox/light-v11',
-    center: [-117.11056697395156, 32.87827887583728],
-    zoom: 8.5
+    center: [-117.11056697395156, 32.97857887589738],
+    zoom: 8.0
   })
   map1.addControl(new mapboxgl.FullscreenControl());
   map1.on('load', async () => {
@@ -42,15 +74,9 @@ onMounted(() => {
       type: 'geojson',
       data: tractsGeojson
     })
-    map1.addSource('transit_routes', {
-      type: 'geojson',
-      data: routesGeojson
-    });
-    map1.addSource('transit_stops', {
-      type: 'geojson',
-      data: stopsGeojson
-    });
+
     map1.addLayer({
+      // change to number of stops
       id: 'tracts-fill',
       type: 'fill',
       source: 'tracts',
@@ -66,32 +92,13 @@ onMounted(() => {
           4560, '#2c7fb8',  // Highest values
           5590, '#253494'
         ],
-        'fill-opacity': 0.7,
+        'fill-opacity': 0.9,
         // Optional: Add outline when hovered
         'fill-outline-color': '#000'
       }
     })
 
-    map1.addLayer({
-      id: 'tracts-outline',
-      type: 'line',
-      source: 'tracts',
-      paint: {
-        'line-color': '#000',
-        'line-width': 0.5
-      }
-    })
 
-    map1.addLayer({
-      id: 'routes',
-      type: 'line',
-      source: 'transit_routes',
-      paint: {
-        'line-color': '#0080ff',
-        'line-width': 2,
-        'line-opacity': 0.7
-      }
-    })
 
 
     map1.on('mousemove', (event) => {
@@ -125,4 +132,3 @@ onMounted(() => {
   })
 })
 </script>
-
